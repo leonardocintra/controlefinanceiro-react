4cc0ad787d323f77cae11feabe566d7e73caaffe
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-05-23 23:00:16 -0300

    corrigido nome da pasta para common e sidebar

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,10 +1,12 @@
-import '../commom/template/dependencies'
+import '../common/template/dependencies'
 import React from 'react'
 
-import Header from '../commom/template/header'
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sideBar'
 
 export default props => (
   <div className="wrapper">
     <Header></Header>
+    <Sidebar></Sidebar>
   </div>
 )
